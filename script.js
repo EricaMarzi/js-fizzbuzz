ab55bcd20c0e60febeaa7b597820c76c37405afc
EricaMarzi <erica.marzi.1993@gmail.com>
2023-12-20 17:21:07 +0100

    rules

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("JS OK")
+
+/* 
+- Scrivi i numeri da 1 a 100 in console
+- Dai una variante (fizz) ai multipli di 3
+- Dai una variante (buzz) ai multipli di 5
+- Dai una variante (fizzbuzz) ai multipli di 3 e 5
+- Stampa i tre punti precedenti in console
+
+- Stampa in pagina in un contenitore
+
+- Applica uno stile tramite css
+- Applica le classi corrette alle varianti 
+*/
