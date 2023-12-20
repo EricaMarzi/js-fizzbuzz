2ae879ed41510ad22ec8819abe312f04ff6cb6f3
EricaMarzi <erica.marzi.1993@gmail.com>
2023-12-20 17:37:16 +0100

    FizzBuzz in console

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ console.log("JS OK")
 - Applica uno stile tramite css
 - Applica le classi corrette alle varianti 
 */
+
+// Da 1 a 100 in console
+for (let i = 1 ; i <= 100 ; i++) {
+    
+    if (i % 3 === 0 && i % 5 === 0) { //Multipli di 3 e 5
+        console.log("FizzBuzz", i)
+    } else if (i % 5 === 0) { //Multipli di 5
+        console.log("Buzz", i)
+    } else if (i % 3 === 0) { //Multipli di 3
+        console.log("Fizz", i)
+    } else {
+        console.log(i)
+    }
+}
