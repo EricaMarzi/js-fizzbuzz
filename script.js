ffdc9b5000e99b76710d5d04cf748ca3b016e20d
EricaMarzi <erica.marzi.1993@gmail.com>
2023-12-20 19:33:43 +0100

    stampa in pagina

--- script.js
+++ script.js
@@ -13,16 +13,28 @@ console.log("JS OK")
 - Applica le classi corrette alle varianti 
 */
 
+// Richiamo gli elementi da html
+const container = document.getElementById("fizzbuzz-container");
+
+let listItem = "<ul>";
+
 // Da 1 a 100 in console
 for (let i = 1 ; i <= 100 ; i++) {
-    
+
     if (i % 3 === 0 && i % 5 === 0) { //Multipli di 3 e 5
-        console.log("FizzBuzz", i)
+        console.log("FizzBuzz", i);
+        listItem += `<li>FizzBuzz</li>`;
     } else if (i % 5 === 0) { //Multipli di 5
-        console.log("Buzz", i)
+        console.log("Buzz", i);
+        listItem += `<li>Buzz</li>`;
     } else if (i % 3 === 0) { //Multipli di 3
-        console.log("Fizz", i)
+        console.log("Fizz", i);
+        listItem += `<li>Fizz</li>`;
     } else {
-        console.log(i)
+        console.log(i);
+        listItem += `<li>${i}</li>`;
     }
+
 }
+listItem += "</ul>";
+container.innerHTML += listItem;
